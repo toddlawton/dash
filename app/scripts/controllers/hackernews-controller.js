2ee79f779a7fd9d7e04ee4191f4459a81256dbbb
Todd Beauchamp <toddbee@gmail.com>
2014-04-12 11:53:30 -0400

    Sort news posts by points

--- app/scripts/controllers/hackernews-controller.js
+++ app/scripts/controllers/hackernews-controller.js
@@ -1,6 +1,10 @@
 app.controller('HackerNewsController', function($scope, $http) {
-	$http.jsonp('http://api.ihackernews.com/page?format=jsonp&callback=JSON_CALLBACK').success(function(data) {
+	$http.jsonp('http://api.ihackernews.com/page?format=jsonp&callback=JSON_CALLBACK')
+	.success(function(data) {
 		$scope.posts = data;
 		$scope.isLoaded = true;
+	})
+	.error(function(data, status) {
+		console.log(data+' '+status);
 	});
 });
